3aea012d990bb53e60a2062bd25c87c42bfe2010
Matthew Ellams <me15262@it025961.users.bris.ac.uk>
2015-10-27 14:45:37 +0000

    Added blank files for UI and AI code PROPERLY

---
+++ Prototype/src/AI.h
@@ -0,0 +1,8 @@
+#ifndef _GENERIC_INCLUDED
+#include "generic.h"
+#endif
+#ifndef _GAME_OBJECTS_INCLUDED
+#include "game_objects.h"
+#endif
+
+#define _AI_INCLUDED 1
---
+++ Prototype/src/UI.h
@@ -0,0 +1,9 @@
+#ifndef _GENERIC_INCLUDED
+#include "generic.h"
+#endif
+#ifndef _GAME_OBJECTS_INCLUDED
+#include "game_objects.h"
+#endif
+#ifndef _AI_INCLUDED
+#include "AI.h"
+#endif
