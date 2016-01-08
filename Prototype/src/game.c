018e0e12a8b20da2f810bffb255f088696099cb7
Alexander Lorimer <al15273@it025960.users.bris.ac.uk>
2016-01-08 15:16:04 +0000

    Rain drops are now far more visible, and the colour change to the whole screen isn't as dark anymore, based on popular preference

--- Prototype/src/game.c
+++ Prototype/src/game.c
@@ -173,6 +173,15 @@ static void createGameUI(GameData *gameData){
   UIConfigure_PercPosition(element, &element->actions[1],1.0,0.0,-dm.w,0,0);
   UIElement_Reparent(element,gameData->uiData.root);
 
+  /* item selected box
+  element = UIElement_Create(0,0,SCORE_COUNTER_WIDTH,TOP_BAR_HEIGHT,4);
+  UIConfigure_FillRect(element,&element->actions[0],255,255,255);
+  UIConfigure_DisplayNumber(element, &element->actions[1], 0,0);
+  UIConfigure_ResourceCounter(element, &element->actions[2],1,&element->actions[1]);
+  UIConfigure_PercPosition(element, &element->actions[3],1.0,0.0,-150,0,0);
+  UIElement_Reparent(element,gameData->uiData.root);*/
+
+
   /* Score counter */
   element = UIElement_Create(0,0,150,30,4);
   UIConfigure_FillRect(element,&element->actions[0],255,255,255);
